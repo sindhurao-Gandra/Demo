3732eab147db496f618032f7c8ee7aee417c2add
sindhurao-Gandra <sindhu.gandra12@gmail.com>
2019-10-17 16:03:21 +0530

    adding first-app

---
+++ first-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
+
+  styles:[' p{color:orange;}']
+ 
+})
+export class AppComponent {
+  title = 'first-app';
+  name='sindhu';
+}
